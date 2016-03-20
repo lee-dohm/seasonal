185a11cdbd5a62ad6f28719df2a28e8205e79406
Lee Dohm <lee-dohm@github.com>
2016-03-20 01:33:41 -0700

    :memo: Add some documentation for the new top-level module

--- lib/seasonal.ex
+++ lib/seasonal.ex
@@ -1,4 +1,14 @@
 defmodule Seasonal do
+  @moduledoc """
+  A simple worker pool library.
+
+  Allows for the creation of named or unnamed worker pools with a fixed number
+  of concurrent workers. Jobs are queued as either anonymous functions or
+  `{module, function, arguments}` tuples. Jobs can be either synchronous or
+  asynchronous. Exceptions within a job end the job, but do not crash the pool.
+  Additionally, exceptions are backpropagated to the process that scheduled the
+  job.
+  """
   use Application
 
   @doc false
