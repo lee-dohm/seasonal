b5ddd8395b335d6cc389631e5a04fa501552c662
Lee Dohm <lee-dohm@github.com>
2016-04-11 23:10:54 -0700

    :memo:

--- lib/seasonal.ex
+++ lib/seasonal.ex
@@ -2,10 +2,10 @@ defmodule Seasonal do
   @moduledoc """
   A simple worker pool.
 
-  Allows for the creation of named or unnamed worker pools with a fixed number
-  of concurrent workers. Jobs are queued as either anonymous functions or
-  `{module, function, arguments}` tuples. Exceptions within a job end the job,
-  but do not crash the pool.
+  Allows for the creation of worker pools with a fixed number of concurrent
+  workers. Pools can be named or unnamed. Named pools can also be supervised. Jobs
+  are queued as either anonymous functions or `{module, function, arguments}`
+  tuples. Exceptions within a job end the job, but do not crash the pool.
   """
   use Application
 
